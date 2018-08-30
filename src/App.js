b4e086e1bc996223dc311ce179ae418e65b3bf82
kanghk <everafterk@naver.com>
2018-08-30 16:26:09 +0900

    chatkit에 유저 등록!

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react'
 import UsernameForm from './components/UsernameForm'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this);
+  }
+  onUsernameSubmitted(username){
+    fetch('http://localhost:3001/users',{
+      method: 'post',
+      headers:{
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({username}),
+    })
+    .then(response=>{
+      console.log('success');
+    })
+    .catch(error=>{
+      console.error(error);
+    })
+  }
+
   render() {
-    return <UsernameForm onSubmit={username => alert(username)}/>
+    return <UsernameForm onSubmit={this.onUsernameSubmitted}/>
   }
 }
 
