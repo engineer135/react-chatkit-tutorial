dd72ce82e5310aa18c4b809121cb0063e3ac2f16
kanghk <everafterk@naver.com>
2018-09-04 22:23:56 +0900

    로그인하고, 채팅방에 있는 메세지 긁어오는 것까지 완성!

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,23 +1,62 @@
 import React, {Component} from 'react';
-import Chatkit from '@pusher/chatkit-server';
+import { ChatManager, TokenProvider } from '@pusher/chatkit';
+import MessageList from './components/MessageList';
 
 class ChatScreen extends Component {
+    constructor(){
+        super();
+        this.state = {
+            messages: []
+        }
+    }
+
     componentDidMount(){
         // 컴포넌트가 화면에 나타나게 됐을때 호출
         // 주로 d3처럼 dom을 사용해야하는 외부 라이브러리 연동을 하거나
         // axios, fetch 등을 통해 ajax 요청을 하거나
         // dom의 속성을 읽거나 직접 변경하는 작업 진행
-        const chatManager = new Chatkit.chatManager({
+        /* const chatManager = new Chatkit.chatManager({
             instanceLocator: 'v1:us1:57303ea2-09c5-4bef-8221-12d7b37b21e7',
             userId: this.props.currentUsername,
             tokenProvider: new Chatkit.TokenProvider({
                 url: 'http://localhost:3001/authenticate'
             })
+        }); 
+        이렇게 했더니 안돼서, 아래처럼 수정함. 
+        yarn add @pusher/chatkit
+        import { ChatManager, TokenProvider } from '@pusher/chatkit'
+        그리고 아래처럼.. 영상을 잘못 본건지 내가 잘못 본건지.. chatkit-server가 아니라 chatkit을 임포트해와야 하더라 흠...
+        
+        */
+
+        const tokenProvider = new TokenProvider({
+            url: `https://us1.pusherplatform.io/services/chatkit_token_provider/v1/57303ea2-09c5-4bef-8221-12d7b37b21e7/token`
+        })
+
+        const chatManager = new ChatManager({
+            instanceLocator: 'v1:us1:57303ea2-09c5-4bef-8221-12d7b37b21e7',
+            userId: this.props.currentUsername,
+            tokenProvider: tokenProvider
         });
 
         chatManager
             .connect()
-            .then(currentUser => console.log('currentUser', currentUser))
+            .then(currentUser => {
+                console.log(currentUser);
+                return currentUser.subscribeToRoom({
+                 roomId:15410205, // 여기 룸아이디는, 챗킷 사이트에서 create new room 한뒤 나온 아이디를 입력해준다..
+                 messageLimit:100,
+                 hooks: {
+                     onNewMessage: message => {
+                         console.log(message);
+                         this.setState({
+                            messages: [...this.state.messages, message]
+                         })
+                     }
+                 }
+             })   
+            })
+            .then(currentRoom => {})
             .catch(error => console.error(error));
     }
 
@@ -25,7 +64,7 @@ class ChatScreen extends Component {
         return(
             <div>
                 <h1>Chat</h1>
-                <p>Hello, {this.props.currentUsername}</p>
+                <MessageList messages={this.state.messages}/>
             </div>
         )
     }
